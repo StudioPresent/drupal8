daf736dffc541be03abe2d7e440cd90b793396af
Goran Nikolovski <nikolovski84@gmail.com>
2017-01-05 22:22:20 +0100

    Add drush install

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -57,8 +57,9 @@ class ScriptHandler {
       $event->getIO()->write("Create a sites/default/files directory with chmod 0777");
     }
     
-    $build_cmd = "drush site-install --account-name=gorann --account-pass=123456 --db-url=mysql://root:Zondara#3@localhost/drupaltest";
-    $shell_output = shell_exec($build_cmd);
+    $install_cmd = "drush site-install --account-name=gorann --account-pass=123456 --db-url=mysql://root:Zondara#3@localhost/drupaltest";
+    $shell_output = shell_exec($install_cmd);
+    $event->getIO()->write($shell_output);
   }
 
   /**
